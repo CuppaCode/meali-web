9c2f1bbefb0b84b043c6d36455648e0d42f4b96a
jwijnen <jeewee94@gmail.com>
2022-10-12 21:40:38 +0200

    create Listing dbseed & factory

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => fake()->word(),
+            'description' => fake()->text(150),
+            'public' => fake()->boolean(),
+        ];
+    }
+}
