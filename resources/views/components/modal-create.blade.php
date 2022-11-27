56a136ee50443debfa1306488206754da25db3a2
jwijnen <jeewee94@gmail.com>
2022-11-27 22:18:32 +0100

    Update styling listings

--- resources/views/components/modal-create.blade.php
+++ resources/views/components/modal-create.blade.php
@@ -1,33 +1,32 @@
-<!--Modal-->
-@props(['href']);
+@props(['href'])
 
-<div id="createListing" class="relative z-10 h-modal" aria-labelledby="create-listing" tabindex="-1" aria-hidden="true" role="dialog" aria-modal="true">
-    <!--
-      Background backdrop, show/hide based on modal state.
-  modal-title
-      Entering: "ease-out duration-300"
-        From: "opacity-0"
-        To: "opacity-100"
-      Leaving: "ease-in duration-200"
-        From: "opacity-100"
-        To: "opacity-0"
-    -->
-    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-  
-    <div class="fixed inset-0 z-10 overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <!--
-            Modal panel, show/hide based on modal state.
-    
-            Entering: "ease-out duration-300"
-                From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                To: "opacity-100 translate-y-0 sm:scale-100"
-            Leaving: "ease-in duration-200"
-                From: "opacity-100 translate-y-0 sm:scale-100"
-                To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            -->
-            <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+<div class="relative z-10" @keydown.window.escape="open = false" x-show="open" aria-labelledby="modal-title" x-ref="dialog" role="dialog" aria-modal="true" x-show="open">
+
+
+  <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" 
+    x-show="open"
+    x-transition:enter="transition ease-out duration-300"
+    x-transition:enter-start="opacity-0"
+    x-transition:enter-end="opacity-100"
+    x-transition:leave="transition ease-in duration-200 "
+    x-transition:leave-start="opacity-100 "
+    x-transition:leave-end="opacity-0 scale-100"></div>
+
+  <div class="fixed inset-0 z-10 overflow-y-auto">
+    <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+
+      <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+      x-show="open"
+      x-transition:enter="ease-out duration-300" 
+      x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" 
+      x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" 
+      x-transition:leave="ease-in duration-200" 
+      x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" 
+      x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" 
+      x-description="Modal panel, show/hide based on modal state." 
+      @click.away="open = false"
+      >
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                     <div class="sm:flex sm:items-start">
                         <x-create-listing-form/>
                     </div>
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -5,104 +5,84 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-7xl container space-x-4 mx-auto flex items-center mt-0 py-4">
 
-            
-
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    Listings
-
-                    @if ( count( $listings ) > 0 ) 
+        <div class="w-full md:w-3/4 flex flex-col">
                     
-                    <div class="flex flex-col">
-                        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                                <div class="overflow-hidden">
-                                <table class="min-w-full">
-                                    <thead class="border-b">
-                                    <tr>
-                                        <th scope="col" class="text-sm font-bold text-gray-900 px-6 py-4 text-left">
-                                            #
-                                        </th>
-                                        <th scope="col" class="text-sm font-bold text-gray-900 px-6 py-4 text-left">
-                                            User
-                                        </th>
-                                        <th scope="col" class="text-sm font-bold text-gray-900 px-6 py-4 text-left">
-                                            Name
-                                        </th>
-                                        <th scope="col" class="text-sm font-bold text-gray-900 px-6 py-4 text-left">
-                                            Description
-                                        </th>
-                                        <th scope="col" class="text-sm font-bold text-gray-900 px-6 py-4 text-left">
-                                            Public
-                                        </th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($listings as $listing)
-                                            <tr class="border-b">
-                                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                                    {{ $loop->index + 1 }}
-                                                </td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                    {{ $listing->user->name }}
-                                                </td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                    {{ $listing->title }}
-                                                </td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4">
-                                                    {{ $listing->description }}
-                                                </td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                    {{ $listing->public ? 'Yes' : 'No' }}
-                                                </td>
-                                                <td class="text-sm text-gray-900 font-light px-6 py-4">
-                                                    
+            <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+                @if ( count( $listings ) > 0 ) 
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-grey dark:bg-white-800">
+                        Listings
+                        <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite products designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and more.</p>
+                    </caption>
 
-                                                    <div x-data="{ open: false }">
-                                                        <span class="cursor-pointer text-xs" @click="open = ! open">
-                                                            <x-heroicons::outline.trash class="w-5 h-5"/>
-                                                        </span>
-                                                        
-                                                                                                              
-                                                        <x-modal-warning href="{{ route('delete.listing', ['id' => $listing->id ])}}"/>
-                                                        
-                                                       
-                                                    </div>
-                                                    
-                                                    {{-- <x-modal-warning/> --}}
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    @endif
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-grey-100 dark:text-gray-800">
+                        <tr>
+                            <th scope="col" class="py-3 px-6">
+                                #
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Name
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Description
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Public
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Actions
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($listings as $listing)
+                        <tr class="bg-white dark:bg-white-800 dark:border-gray-700">
+                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-gray-400">
+                                {{ $loop->index + 1 }}
+                            </th>
+                            <td class="py-4 px-6">
+                                {{ $listing->title }}
+                            </td>
+                            <td class="py-4 px-6">
+                                {{ $listing->description }}
+                            </td>
+                            <td class="py-4 px-6">
+                                {{ $listing->public ? 'Y' : 'N' }}
+                            </td>
+                            <td class="py-4 px-6">
+                                <span x-data="{ open: false }">
+                                    <x-heroicons::outline.trash @click="open = ! open" class="cursor-pointer w-5 h-5"/>
+                                                                                            
+                                    <x-modal-warning href="{{ route('delete.listing', ['id' => $listing->id ])}}"/>
+                                </span>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                @endif
                     
-                    <div class="flex items-center pt-5">
-                        Create new listing
+                <div class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-grey dark:bg-white-800" x-data="{ open: false }" class="ml-3">
+                    
+                    <button type="button" @click="open = ! open" class="text-white bg-orange-400 hover:bg-orange-500 border border-transparent font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
+                        Create listing
+                        <svg aria-hidden="true" class="ml-2 -mr-1 w-5 h-5"" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </button>
 
-                        <div x-data="{show:false}" class="ml-3">
-                            <button type="button" @click="show=!show" class="text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2">
-                                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                <span class="sr-only">Icon description</span>
-                            </button>
-
-                            <template x-if="show">
-                                <x-modal-create/>
-                            </template>
-                        </div>
-                    </div>
-                
+                    <x-modal-create/>
                 </div>
             </div>
-            
+        </div>
+
+        <div class="w-full md:w-1/4 flex flex-col">
+
+            <div class="overflow-x-auto relative shadow-md sm:rounded-lg bg-white">
+
+                test
+                
+            </div>
         </div>
 
     </div>
